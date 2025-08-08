ff6d8c193bdc4e2057c928b44957468239fd26cf
Curtis Muir <49688644+curtismu7@users.noreply.github.com>
2025-08-08 10:17:10 -0500

    🔖 Update version to 7.0.1.3

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 // Application version - SINGLE SOURCE OF TRUTH
-export const APP_VERSION = '7.0.1.2';
+export const APP_VERSION = '7.0.1.3';
 
 // Helper functions for version formatting
 export function getFormattedVersion() {
